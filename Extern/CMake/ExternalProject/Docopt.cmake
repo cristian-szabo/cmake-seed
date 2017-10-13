3b0cbbe9131bc75f4e918f9e520f72140d5a4fa6
Ioan-Cristian Szabo <ioan-cristian.szabo@arm.com>
2017-10-13 20:52:37 +0100

    cleanup

--- Extern/CMake/ExternalProject/Docopt.cmake
+++
@@ -1,9 +0,0 @@
-include (ExternalProject)
-
-ExternalProject_Add (
-    docopt
-
-    GIT_REPOSITORY "https://github.com/docopt/docopt.cpp.git"
-    GIT_TAG "master"
-       
-    CMAKE_ARGS "-DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>")
--- Lib/Include/Lib.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include "Config.hpp"
-
-#include "Calculator.hpp"
